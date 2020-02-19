9a78f4f761df990ad3729f5d37940eeed72a27df
Michael Madsen <mrma@itu.dk>
2020-02-19 15:15:58 +0100

    updated sketch with class to handle text

--- sketch.js
+++ sketch.js
@@ -1,8 +1,28 @@
 var database
+var text
+var myWords
+
+function preload() {
+  // Preload test.txt to test string-manipulation and later cloudcreation
+  text = loadStrings('test.txt')
+}
+
+function formatData(data, dictionary) {
+  // myWords = []
+  // for (let i = 0; i < dictionary.keys.length; i++) {
+  //   let thisWord = dictionary.keys[i]
+  //   let thisCount = dictionary.getCount(thisWord)
+  //   myWords.push({ word: thisWord, size: thisCount })
+  // }
+  myWords = dictionary.split(data)
+  console.log(myWords)
+  // constructCloud()
+}
 
 function setup() {
   createCanvas(400, 400)
 
+  // -------------- Firebase
   var firebaseConfig = {
     apiKey: 'AIzaSyAET8NX9-hMGX0WeYaZNNI147BMHOL5RII',
     authDomain: 'dream-cloud-d4d1d.firebaseapp.com',
@@ -17,11 +37,88 @@ function setup() {
   firebase.initializeApp(firebaseConfig)
   // firebase.analytics()
   console.log(firebase)
-
   // database = firebase.database()
   // var ref = database.ref('dreams')
+  // -------------- Firebase
+
+  // -------------- Texhandling
+  wordCounter = new TextHandler()
+  formatData(text, wordCounter)
+  // -------------- Texhandling
 }
 
 function draw() {
   background(210)
+}
+
+class TextHandler {
+  constructor() {
+    this.dict = {}
+    this.keys = []
+  }
+
+  // Splitting up the text
+  split(text) {
+    // Split into array of tokens
+    return text.split(/\W+/)
+  }
+
+  // A function to validate a toke
+  validate(token) {
+    return /\w{2,}/.test(token)
+  }
+
+  // Process new text
+  process(tokens) {
+    //console.log(tokens);
+    // For every token
+    for (var i = 0; i < tokens.length; i++) {
+      // Lowercase everything to ignore case
+      if (!tokens[i]) continue
+      var token = tokens[i].toLowerCase()
+      if (this.validate(token)) {
+        // Increase the count for the token
+        this.increment(token)
+      }
+    }
+  }
+
+  // An array of keys
+  getKeys() {
+    return this.keys
+  }
+
+  // Get the count for a word
+  getCount(word) {
+    return this.dict[word]
+  }
+
+  // Increment the count for a word
+  increment(word) {
+    // Is this a new word?
+    if (!this.dict[word]) {
+      this.dict[word] = 1
+      this.keys.push(word)
+      // Otherwise just increment its count
+    } else {
+      this.dict[word]++
+    }
+  }
+
+  // Sort array of keys by counts
+  sortByCount() {
+    // For this function to work for sorting, I have
+    // to store a reference to this so the context is not lost!
+    var concordance = this
+
+    // A fancy way to sort each element
+    // Compare the counts
+    function sorter(a, b) {
+      var diff = concordance.getCount(b) - concordance.getCount(a)
+      return diff
+    }
+
+    // Sort using the function above!
+    this.keys.sort(sorter)
+  }
 }
